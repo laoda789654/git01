9c9e4c5627e29ed656ff94d19b8e992b41a9b958
ASUS <842607292@qq.com>
2020-09-09 16:49:01 +0800

    创建了一个项目

---
+++ git01/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+
+    public void hello(){
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
