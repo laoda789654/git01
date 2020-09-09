cc590bffa1477b52263156c25f3acdee2bbfae24
ASUS <842607292@qq.com>
2020-09-09 17:00:57 +0800

    Revert "修改了hello文件"
    This reverts commit 6e41a1c40b4e1fbeaff9b48ddbb8b0ad3640690c.

--- git01/src/main/java/Hello.java
+++ git01/src/main/java/Hello.java
@@ -7,6 +7,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("");
+        System.out.println("hi"+"");
+        System.out.println("你好");
     }
 
 }
